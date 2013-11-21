fe0cac48d7ddcba8d6794c440b3faafdc94fc80c
gpoisoned <gpoisoned@gmail.com>
2013-11-20 18:13:47 -0700

    Creatd new rails app

---
+++ Facebook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facebook::Application.config.secret_token = '87f293c17d512b0194c60d24c7ae1b760841b20e166061da786bd5ad53db8b4537534bd676bc68ac765e0c330aa16d63c9d664a2c5087a0337d5ce484e1a60ba'
