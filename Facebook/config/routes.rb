83e8e6d304b9c3d1d7b0e8f5a2f9f0271432ed89
saasbook <saasbook@saasbook.(none)>
2013-12-03 17:21:45 -0800

    set login page to /users route

--- Facebook/config/routes.rb
+++ Facebook/config/routes.rb
@@ -7,7 +7,7 @@ Facebook::Application.routes.draw do
 
   resources :profiles
 
-
+  root :to => redirect("/users")
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
