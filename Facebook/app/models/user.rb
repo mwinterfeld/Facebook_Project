870f6e8a87068ddb78edade8fbf2b07d892db280
saasbook <saasbook@saasbook.(none)>
2013-12-08 18:18:33 -0800

    added attr_accessible :friends

--- Facebook/app/models/user.rb
+++ Facebook/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :friend_count, :id, :post_count, :username, :password
-  serialize :friends  
-  has_one:profile
-  has_many:posts
+  serialize :friends
+  attr_accessible :friends 
+  has_one :profile
+  has_many :posts
 end
