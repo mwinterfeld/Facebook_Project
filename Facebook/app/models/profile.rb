54eb1f3a95d1adef8baad1e87ef6681ce875102a
gpoisoned <gpoisoned@gmail.com>
2013-12-11 16:04:51 -0700

    Fixed mass assignment error for interest

--- Facebook/app/models/profile.rb
+++ Facebook/app/models/profile.rb
@@ -1,5 +1,5 @@
 class Profile < ActiveRecord::Base
-  attr_accessible :city, :country, :first_name, :last_name, :user_id
+  attr_accessible :city, :country, :first_name, :last_name, :interests, :user_id
   serialize :interests
   belongs_to :user
   def user
