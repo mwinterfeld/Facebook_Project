b7dfd135ce88f662dcec51689c1bccd2dcee2d9c
mwinterfeld <michael.winterfeld@colorado.edu>
2013-12-12 21:17:50 -0800

    Added 1 more rspec test

--- Facebook/spec/models/profile_spec.rb
+++ Facebook/spec/models/profile_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 require 'profile'
+require 'user'
 
 describe Profile do
   let(:profile1){Profile.new(:first_name => "jim", :last_name => "jones", :country => "qatar", :username => "jimjones")}
   let(:profile2){Profile.new(:first_name => "john", :last_name => "jones", :country => "uae", :username => "johnjones")}
+  let(:user){User.new}
 
   it "should have a first name, last name, and a country of origin" do
   	profile1.first_name.should match(/^jim$/)
@@ -13,5 +15,12 @@ describe Profile do
   	profile2.last_name.should match(/^jones$/)
   	profile2.country.should match(/^uae$/)
 
+  end
+
+  it "should be able to add a profile to a user" do
+    user.profile = profile1
+    user.profile.first_name.should match(/^jim$/)
+    user.profile = profile2
+    user.profile.first_name.should match(/^john$/)
   end
 end
