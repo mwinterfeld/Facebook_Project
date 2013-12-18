ac5a4080acf0ca65b7f74c9ac7b98ce7d925b75a
mwinterfeld <michael.winterfeld@colorado.edu>
2013-12-18 12:30:10 -0800

    more changes

--- config/application.rb
+++ config/application.rb
@@ -54,8 +54,10 @@ module Facebook
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
+    config.assets.initialize_on_precompile = false
     config.assets.enabled = true
 
+    
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
