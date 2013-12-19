756cee8eacc39ae6fe899fe6ed9b1e8d9aa7bef3
mwinterfeld <michael.winterfeld@colorado.edu>
2013-12-18 20:06:02 -0800

    wallposts field added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   attr_accessible :friend_count, :id, :post_count, :username, :password, :user_id
   serialize :friends
   attr_accessible :friends 
+  serialize :wall_posts
+  attr_accessible :wall_posts
   has_one :profile
   has_many :posts
   attr_accessible :posts
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131214235125) do
+ActiveRecord::Schema.define(:version => 20131219040448) do
 
   create_table "posts", :force => true do |t|
     t.string   "content"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(:version => 20131214235125) do
     t.string   "password"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.string   "wall_posts"
   end
 
 end
